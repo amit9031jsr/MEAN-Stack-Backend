5115517ba4bb11eef178792a6114a3f3debd15b0
amit9031jsr <amit9031jsr@gmail.com>
2020-10-16 21:11:58 +0530

    added create Employee

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -9,9 +9,26 @@ router.get('/', (req, res) => {
             res.send(docs);
         }
         else {
-            console.log('Error in Retrieving Employees:' + JSON.stringify(err, undefined, 2));
+            console.log('Error in Retrieving Employees: ' + JSON.stringify(err, undefined, 2));
         }
     });
-})
+});
+
+router.post('/', (req, res) => {
+    var emp = new Employee({
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    });
+    emp.save((err, docs) => {
+        if(!err) {
+            res.send(docs);
+        }
+        else {
+            console.log('Error in Employee Save: ' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
 
 module.exports = router;
