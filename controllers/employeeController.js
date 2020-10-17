45b51698e947ad4019db3654c2f1913aadd1d28e
amit9031jsr <amit9031jsr@gmail.com>
2020-10-17 15:34:23 +0530

    added delete employee data

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -69,4 +69,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
+
+// Delete employee data
+router.delete('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id: ${req.params.id}`);
+    
+    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.send(doc);
+        } else {
+            console.log('Error in Employee Delete: ' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
 module.exports = router;
