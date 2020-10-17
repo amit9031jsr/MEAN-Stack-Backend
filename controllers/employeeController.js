574c0c9a576be7b516cdf90e75a442b091f116cb
amit9031jsr <amit9031jsr@gmail.com>
2020-10-17 10:48:20 +0530

    added retreive all employees data

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,19 +1,11 @@
 const express = require('express');
 var router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
 
 var { Employee } = require('../models/employee.js');
 
-router.get('/', (req, res) => {
-    Employee.find((err, docs) => {
-        if(!err) {
-            res.send(docs);
-        }
-        else {
-            console.log('Error in Retrieving Employees: ' + JSON.stringify(err, undefined, 2));
-        }
-    });
-});
 
+// Create new employee
 router.post('/', (req, res) => {
     var emp = new Employee({
         name: req.body.name,
@@ -21,12 +13,23 @@ router.post('/', (req, res) => {
         office: req.body.office,
         salary: req.body.salary
     });
-    emp.save((err, docs) => {
-        if(!err) {
-            res.send(docs);
-        }
-        else {
+    emp.save((err, doc) => {
+        if (!err) {
+            res.send(doc);
+        } else {
             console.log('Error in Employee Save: ' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+
+// Retreive all employee details
+router.get('/', (req, res) => {
+    Employee.find((err, docs) => {
+        if (!err) {
+            res.send(docs);
+        } else {
+            console.log('Error in Retrieving Employees: ' + JSON.stringify(err, undefined, 2));
         }
     });
 });
