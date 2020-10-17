c0504fa5b74f310affa97d1b2600bb7b54568a46
amit9031jsr <amit9031jsr@gmail.com>
2020-10-17 11:17:08 +0530

    added update employee data

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -48,4 +48,25 @@ router.get('/:id', (req, res) => {
     });
 });
 
+
+// Update employee details
+router.put('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id: ${req.params.id}`);
+
+    var emp = {
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    };
+    Employee.findByIdAndUpdate(req.params.id, {$set: emp}, {new: true}, (err, doc) => {
+        if (!err) {
+            res.send(doc);
+        } else {
+            console.log('Error in Employee Update: ' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
 module.exports = router;
